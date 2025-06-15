ac7854e325578fd46f1eee031c8f531b9d1c396e
toledomg <toledomg@yahoo.com.br>
2025-06-15 13:57:43 -0300

    Initial commit with project setup and basic structure established.

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM python:3.10-slim
+
+WORKDIR /app
+
+RUN apt update &&
+    apt install -y tesseract-ocr tesseract-ocr-por libglib2.0-0 libsm6 libxext6 libxrender-dev &&
+    pip install flask pillow pytesseract
+
+COPY ocr-api.py /app/ocr-api.py
+
+EXPOSE 9000
+
+CMD ["python", "/app/ocr-api.py"]
---
+++ ocr-api.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, jsonify
+import pytesseract
+from PIL import Image
+import os
+
+app = Flask(__name__)
+
+
+@app.route("/ocr", methods=["POST"])
+def ocr():
+    if "file" not in request.files:
+        return jsonify({"error": "No file"}), 400
+    img_file = request.files["file"]
+    path = "/tmp/image.png"
+    img_file.save(path)
+    try:
+        text = pytesseract.image_to_string(
+            Image.open(path), lang="por"
+        )  # Use "eng" para inglês, "por" para português
+    except Exception as e:
+        os.remove(path)
+        return jsonify({"error": str(e)}), 500
+    os.remove(path)
+    return jsonify({"text": text})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=9000)
